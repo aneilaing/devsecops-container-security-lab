55da18cc52496fa4340cccc9c73aba1ae20f3431
Aneika <aneika.laing@hotmail.com>
2026-04-08 22:36:28 -0400

    Add DAST scanning and remediate security headers using Helmet

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const app = express();
+
+//Apply security headers
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('DevSecOps Lab Running');
